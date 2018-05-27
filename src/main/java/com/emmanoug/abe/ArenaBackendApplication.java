2eeee813302a239db39fd3bd98d358d967846026
Georgios <geo.emm@hotmail.com>
2018-05-27 22:51:06 +0300

    Initial commit

---
+++ src/main/java/com/emmanoug/abe/ArenaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.emmanoug.abe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArenaBackendApplication.class, args);
+	}
+}
